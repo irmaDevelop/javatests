e384b1c688b5123d0a22cf2d24816e9a8cc21d53
IrmaJulia <irmajuliadavila@icloud.com>
2019-02-21 12:08:44 -0500

    Second Commit

---
+++ src/main/java/com/miempresa/javatests/util/StringUtil.java
@@ -0,0 +1,7 @@
+package com.miempresa.javatests.util;
+
+public class StringUtil {
+    public String repeat(String str, int times){
+        return null;
+    }
+}
---
+++ src/test/java/com/miempresa/javatests/util/StringUtilTest.java
@@ -0,0 +1,7 @@
+package com.miempresa.javatests.util;
+
+/*import static org.junit.Assert.*;
+*/
+public class StringUtilTest {
+
+}
