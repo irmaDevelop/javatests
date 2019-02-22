18d0994a5cb026e7e943fd0afccd69a327d7d4e5
IrmaJulia <irmajuliadavila@icloud.com>
2019-02-22 09:58:59 -0500

    Firs TEST

--- src/test/java/com/miempresa/javatests/util/StringUtilTest.java
+++ src/test/java/com/miempresa/javatests/util/StringUtilTest.java
@@ -3,5 +3,21 @@ package com.miempresa.javatests.util;
 /*import static org.junit.Assert.*;
 */
 public class StringUtilTest {
+    //psvm public static void main + tab
+    public static void main(String[] args) {
 
+        String result = StringUtil.repeat("hola", 3);
+        System.out.println(result);
+
+        if (result.equals("holaholahola")){
+            System.out.println("Ok");
+        }
+
+        String result2 = StringUtil.repeat("hola", 1);
+        System.out.println(result2);
+
+        if (result2.equals("hola")){
+            System.out.println("Ok");
+        }
+    }
 }
